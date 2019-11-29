305c495352aeeeb75c0151d853d733c0457597a1
lidianxiang <ldx2703@gmail.com>
2019-11-29 10:47:04 +0800

    删除排序链表中的重复元素II

---
+++ 链表/82-删除排序链表中的重复元素II.py
@@ -0,0 +1,57 @@
+"""
+给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
+
+示例 1:
+
+输入: 1->2->3->3->4->4->5
+输出: 1->2->5
+示例 2:
+
+输入: 1->1->1->2->3
+输出: 2->3
+
+"""
+
+
+# definition for singly-linked list
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+# 递归法
+class Solution:
+    def deleteDuplicates(self, head):
+        if not head:
+            return head
+
+        if head.next and head.val == head.next.val:
+            while head.next and head.val == head.next.val:
+                head = head.next
+            return self.deleteDuplicates(head.next)
+        else:
+            head.next = self.deleteDuplicates(head.next)
+        return head
+
+
+class Solution2:
+    """迭代 快慢指针,用快指针跳过那些有重复数组,慢指针负责和快指针拼接"""
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if head == None or head.next == None:
+            return head
+        dummy = ListNode(-1)
+        dummy.next = head
+        slow = dummy
+        fast = dummy.next
+        while fast:
+            if fast.next and fast.next.val == fast.val:
+                tmp = fast.val
+                while fast and tmp == fast.val:
+                    fast = fast.next
+            else:
+                slow.next = fast
+                slow = fast
+                fast = fast.next
+        slow.next = fast
+        return dummy.next
