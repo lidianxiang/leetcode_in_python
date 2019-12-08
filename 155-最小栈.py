fd4eb6451002ad3aaa68482165084fe39d215227
lidianxiang <ldx2703@gmail.com>
2019-12-08 15:37:45 +0800

    155-最小栈

---
+++ 155-最小栈.py
@@ -0,0 +1,49 @@
+"""
+设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+
+push(x) -- 将元素 x 推入栈中。
+pop() -- 删除栈顶的元素。
+top() -- 获取栈顶元素。
+getMin() -- 检索栈中的最小元素。
+示例:
+
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+
+"""
+
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.data = []
+        self.helper = []
+
+    def push(self, x: int) -> None:
+        self.data.append(x)
+        if len(self.helper) == 0 or x <= self.helper[-1]:
+            self.helper.append(x)
+        else:
+            self.helper.append(self.helper[-1])
+
+    def pop(self) -> None:
+        if self.data:
+            self.helper.pop()
+        return self.data.pop()
+
+    def top(self) -> int:
+        if self.data:
+            return self.data[-1]
+
+    def getMin(self) -> int:
+        if self.helper:
+            return self.helper[-1]
