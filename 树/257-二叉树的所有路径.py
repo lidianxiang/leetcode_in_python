e35197a0fc97452ea5b86c7a380e6ae9bce78480
lidianxiang <ldx2703@gmail.com>
2019-12-12 20:53:34 +0800

    257-二叉树的所有路径

---
+++ 树/257-二叉树的所有路径.py
@@ -0,0 +1,64 @@
+"""
+给定一个二叉树，返回所有从根节点到叶子节点的路径。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例:
+
+输入:
+
+   1
+ /   \
+2     3
+ \
+  5
+
+输出: ["1->2->5", "1->3"]
+
+解释: 所有根节点到叶子节点的路径为: 1->2->5, 1->3
+
+"""
+
+
+# definition a binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    """最简单的方法就是：递归"""
+    def binaryTreePath(self, root):
+        def construct_paths(root, path):
+            if root:
+                path += str(root.val)
+                if not root.left and not root.right:
+                    paths.append(path)
+                else:
+                    path += '->'
+                    construct_paths(root.left, path)
+                    construct_paths(root.right, path)
+        paths = []
+        construct_paths(root, '')
+        return paths
+
+
+class Solution2:
+    """迭代方式"""
+    def binaryTreePaths(self, root):
+        if not root:
+            return []
+
+        paths = []
+        stack = [(root, str(root.val))]
+        while stack:
+            node, path = stack.pop()
+            if not node.left and not node.right:
+                paths.append(path)
+            if node.left:
+                stack.append((node.left, path + '->' + str(node.left.val)))
+            if node.right:
+                stack.append((node.right, path + '->' + str(node.right.val)))
+        return paths
