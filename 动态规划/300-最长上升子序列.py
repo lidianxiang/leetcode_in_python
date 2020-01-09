5b604529f870467ed2d11ab9c86bec01b905c2e2
lidianxiang <ldx2703@gmail.com>
2020-01-09 21:44:54 +0800

    300-最长上升子序列

---
+++ 动态规划/300-最长上升子序列.py
@@ -0,0 +1,43 @@
+"""
+给定一个无序的整数数组，找到其中最长上升子序列的长度。
+
+示例:
+
+输入: [10,9,2,5,3,7,101,18]
+输出: 4
+解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+说明:
+
+可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可。
+你算法的时间复杂度应该为 O(n2) 。
+"""
+
+
+class Solution:
+    def lengthOfLIS(self, nums):
+        if not nums:
+            return 0
+        dp = [1] * len(nums)
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+
+
+class Solution2:
+    """动态规划 + 二分查找"""
+    def lengthOfLIS(self, nums):
+        tails, res = [0] * len(nums), 0
+        for num in nums:
+            i, j = 0, res
+            while i < j:
+                m = (i + j) // 2
+                if tails[m] < num:
+                    i = m + 1
+                else:
+                    j = m
+            tails[i] = num
+            if j == res:
+                res += 1
+        return res
