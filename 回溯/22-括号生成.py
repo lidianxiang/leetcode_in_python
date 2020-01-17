eb7c46889a5a74e1d4435bb830a5a73a2e5f6f53
lidianxiang <ldx2703@gmail.com>
2020-01-17 22:01:53 +0800

    22-括号生成

---
+++ 回溯/22-括号生成.py
@@ -0,0 +1,34 @@
+"""
+给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+
+例如，给出 n = 3，生成结果为：
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+"""
+
+
+class Solution:
+    """回溯
+    只有在我们知道序列仍然保持有效时才添加 '(' or ')'，而不是像 方法一 那样每次添加。我们可以通过跟踪到目前为止放置的左括号和右括号的数目来做到这一点，
+
+    如果我们还剩一个位置，我们可以开始放一个左括号。 如果它不超过左括号的数量，我们可以放一个右括号。
+    """
+    def generateParenthesis(self, n):
+        ans = []
+
+        def backtrack(S = '', left=0, right=0):
+            if len(S) == 2 * n:
+                ans.append(S)
+                return
+            if left < n:
+                backtrack(S+'(', left+1, right)
+            if right < left:
+                backtrack(S+')',left, right+1)
+        backtrack()
+        return ans
