291c9b2ee99c762e3f57267ac4dbaca08f2ce20a
lidianxiang <ldx2703@gmail.com>
2020-01-24 12:58:30 +0800

    557-反转字符串中的单词III

---
+++ 字符串/557-反转字符串中的单词III.py
@@ -0,0 +1,18 @@
+"""
+给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+
+示例 1:
+
+输入: "Let's take LeetCode contest"
+输出: "s'teL ekat edoCteeL tsetnoc" 
+注意：在字符串中，每个单词由单个空格分隔，并且字符串中不会有任何额外的空格。
+
+"""
+
+
+class Solution:
+    def reverseWords(self, s):
+        s = s.lstrip().rstrip()
+        words = s.split()
+        reverse_words = [word[::-1] for word in words]
+        return " ".join(reverse_words)
