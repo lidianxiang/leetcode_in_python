69c29fcce7d427597cac1000f706b5e900d8911b
lidianxiang <ldx2703@gmail.com>
2020-01-28 10:43:27 +0800

    81-搜索旋转排序数组II

---
+++ 二分查找/81-搜索旋转排序数组II.py
@@ -0,0 +1,43 @@
+"""
+假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+
+( 例如，数组 [0,0,1,2,2,5,6] 可能变为 [2,5,6,0,0,1,2] )。
+
+编写一个函数来判断给定的目标值是否存在于数组中。若存在返回 true，否则返回 false。
+
+示例 1:
+
+输入: nums = [2,5,6,0,0,1,2], target = 0
+输出: true
+示例 2:
+
+输入: nums = [2,5,6,0,0,1,2], target = 3
+输出: false
+"""
+
+
+class Solution:
+    """二分查找"""
+    def search(self, nums, target):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            # 正好中间值等于目标值
+            if nums[mid] == target:
+                return True
+            if nums[mid] == nums[r] == nums[l]:
+                l += 1
+                r -= 1
+            # 当中间值大于等于左边值
+            elif nums[mid] >= nums[l]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            # 当中间值小于左边值
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return False
