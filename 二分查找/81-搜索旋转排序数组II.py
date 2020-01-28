d82156867b4c07f1b7f047160a36f222e354f9b5
lidianxiang <ldx2703@gmail.com>
2020-01-28 10:46:45 +0800

    81-搜索旋转排序数组II

--- 二分查找/81-搜索旋转排序数组II.py
+++ 二分查找/81-搜索旋转排序数组II.py
@@ -19,25 +19,25 @@
 class Solution:
     """二分查找"""
     def search(self, nums, target):
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            mid = (l + r) // 2
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
             # 正好中间值等于目标值
             if nums[mid] == target:
                 return True
-            if nums[mid] == nums[r] == nums[l]:
-                l += 1
+            if nums[mid] == nums[right] == nums[left]:
+                left += 1
                 r -= 1
             # 当中间值大于等于左边值
-            elif nums[mid] >= nums[l]:
-                if nums[l] <= target < nums[mid]:
-                    r = mid - 1
+            elif nums[mid] >= nums[left]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
                 else:
-                    l = mid + 1
+                    left = mid + 1
             # 当中间值小于左边值
             else:
-                if nums[mid] < target <= nums[r]:
-                    l = mid + 1
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
                 else:
-                    r = mid - 1
+                    right = mid - 1
         return False
