1b232d95958131d7f6bb86bfb412f5e5d338d1b4
lidianxiang <ldx2703@gmail.com>
2020-02-18 20:46:09 +0800

    面试02.03.删除中间节点

---
+++ 链表/面试02.03.删除中间节点.py
@@ -0,0 +1,30 @@
+"""
+实现一种算法，删除单向链表中间的某个节点（除了第一个和最后一个节点，不一定是中间节点），假定你只能访问该节点。
+
+示例：
+
+输入：单向链表a->b->c->d->e->f中的节点c
+结果：不返回任何数据，但该链表变为a->b->d->e->f
+
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        # 特判
+        if not node:
+            return node
+
+        node.val = node.next.val
+        node.next = node.next.next
